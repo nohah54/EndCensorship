845e35c9b2e1cad8467fd51103422d5d9f44a992
nohah54 <stinger54549@gmail.com>
2023-11-04 15:30:01 -0500

    Updated main.js with floating cube lol

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-const scene = new THREE.Scene();
+let scene, camera, renderer, cube;
 
-const camera = new THREE.PerspectiveCamera(
-    75, 
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
+function init() {
+    scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
+    camera = new THREE.PerspectiveCamera(
+        75, 
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    document.body.appendChild(renderer.domElement);
+    
+    const geometry = new THREE.BoxGeometry(2,2,2);
+    const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    
+    camera.position.z = 5;
+}
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+function onRefreshFrame() {
+    requestAnimationFrame(onRefreshFrame); 
 
-document.body.appendChild(renderer.domElement);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+onRefreshFrame();
